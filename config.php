ecf05153bf3cdc3a0f79aff9c71ea6961c335097
joseorteha <joseortegahac@gmail.com>
2025-06-19 00:53:20 -0600

    actualizacion

--- config.php
+++ config.php
@@ -1,5 +1,10 @@
 <?php
-$conexion = new mysqli("localhost", "jose", "jose2025", "socialphp");
+$conexion = new mysqli(
+    "sql309.infinityfree.com",
+    "if0_39269301",
+    "NgDQpWAilTXb3",
+    "if0_39269301_socialphp"
+);
 
 if ($conexion->connect_error) {
     die("Conexión fallida: " . $conexion->connect_error);
